8d2c920f350eb7cad4516da67922f536834d6a0d
nareth347 <nareth441@gmail.com>
2024-01-31 13:44:27 +0700

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
-import { InputForm } from '@/components'
+import { Button } from '@/components'
 const Home = () => {
   return (
     <div>
-      
-      <InputForm width="sm" height="sm" placholder="Enter your email" />
+      <Button
+        rightIcon={
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+            />
+          </svg>
+        }
+        colorScheme="primary"
+        isDisabled={false}
+        className="border rounded-full px-4 py-2 w-[100px]"
+      >
+        Login
+      </Button>
     </div>
   )
 }
--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,1 +1,1 @@
-export * from "./input"
+export {Button} from './button'

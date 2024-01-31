e7efad034b06a4fb54c93abe57159ecbdc0589e0
Smoeury Songvat <vatgaming287@gmail.com>
2024-01-31 15:22:33 +0700

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
+<<<<<<< HEAD
       
+=======
+      <InputForm width="sm" height="sm" placholder="Enter your email"/>
+      <InputForm width="sm" height="sm" placholder="Enter your email" />
+>>>>>>> 3f50cde (Branch: 38-input-form)
     </div>
   )
 }

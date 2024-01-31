62b49d72ee8ac559453aefad20dc0de44b7b522f
Smoeury Songvat <vatgaming287@gmail.com>
2024-01-31 18:25:39 +0700

    new commit

--- src/components/atoms/input/index.ts
+++ src/components/atoms/input/index.ts
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
-export {Inputform} from "./inputform"
-=======
-import { Inputform } from './inputform'
 
+import { Inputform } from './inputform'
 export {Inputform}
->>>>>>> befa638a075fbc8593af8b07df645f3dfec54e05
+
+
+

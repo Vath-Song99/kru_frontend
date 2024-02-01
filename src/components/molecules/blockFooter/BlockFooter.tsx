50eeb53d8135b7ff009dac7dba5bd16d9926eaee
nareth347 <nareth441@gmail.com>
2024-02-01 14:01:47 +0700

    updated

---
+++ src/components/molecules/blockFooter/BlockFooter.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React, {FC, ReactNode} from "react";
+
+interface BlockFooterProps {
+    children: React.ReactNode;
+    className?: string;
+}
+
+const BlockFooter: FC<BlockFooterProps> = ({
+    children,
+    className,
+})
+

9741fc9dafc27e0c45053d7947af79b6e0475cbd
sea porhai <seaporhai@gmail.com>
2024-02-01 10:48:39 +0700

    new commit

--- src/components/molecules/benner/banner.tsx
+++ src/components/molecules/benner/banner.tsx
@@ -5,18 +5,23 @@ import Image from 'next/image'
 const Banner = () => {
 const image =   ''
   return (
-   <>
-  <div className='ml-[20px] border-[#8e44ad] border-solid border-[2px] w-[1410px] h-[538px] mt-[40px]' >
-    <div className='flex font-bold '>
-        <Image src={"/picture/image 16.png"} width={730} height={538} alt="Imgbanner">   
+   <div className='w-screen flex justify-center'>
+  <div className=' border-[#8e44ad] border-solid border-[2px] w-[1410px] h-[538px] mt-[40px]' >
+    <div className='flex space-x-[200px]'>
+        <Image src={"/picture/image 16.png"} width={730} height={539} alt="Imgbanner">   
         </Image>
-        <p className='text-[30px] w-[60px] '>
+        <div className=''>
+        <p className='text-[40px] w-[350px]  font-bold mt-[60px] '>
             Graduated or Want Internship ? Submit now , Show Your Skill
         </p>
+        <button className='bg-[#8e44ad] text-white rounded-md w-[300px] h-[80px] mt-[60px] text-[25px]'>
+            Become a Tutor Now 
+        </button>
+        </div>
        
     </div>
   </div>
-   </>
+   </div>
   )
 }
 

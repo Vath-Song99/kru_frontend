823b8374d09a5cf3980f964cea07fc263b4bcaae
nareth347 <nareth441@gmail.com>
2024-02-03 23:30:55 +0700

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { CardSubject } from "@/components/molecules/cardSubject";
+import { ButtonIcon } from "@/components";
+import { Footer } from "@/components/organisms/footer/footer";
 import React from "react";
-
 const Page = () => {
   return (
     <div>
-        <h1>1-ON-1 TUTORS</h1>
-        <div className = "w-20 h-1 bg-red-400"></div>
+      <Footer></Footer>
+      <Footer></Footer>
     </div>
   );
 };

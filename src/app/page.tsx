393a1a284740c6e556b5393fc88e341cbea63a5c
sea porhai <seaporhai@gmail.com>
2024-02-04 13:09:24 +0700

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Dropdown, Button } from "@/components";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { Dropdown,  Button  } from '@/components'
-
-
-import { Typography,Banner} from '@/components'
+import {
+  Typography,
+  Banner,
+  Navbar,
+  CardSubject,
+  NavbarLeftSever,
+  NavbarRightClient,
+  
+} from "@/components";
 
 const Home = () => {
   return (
-
-    <div >
-      <div className='flex'>
-        <Banner >
-        </Banner>
-         
+    <div>
+      <div className="">
+        
       </div>
     </div>
+  );
+};
 
-=======
-const Page = () => {
-  return (
-    <div>page</div>
->>>>>>> b4a83bd9ecb272d706b44bac10488a317603c4a1
-=======
-const Page = () => {
-  return (
-    <div className='w-full flex justify-center'>
-        
-    </div>
->>>>>>> 5451c01af75ee3dd05fcafe9100ffa9f97d1be63
-  )
-}
-
-export default Page
+export default Home;

20930f91219cc4280b6756659bdc833f7d5838e6
Smoeury Songvat <vatgaming287@gmail.com>
2024-02-05 09:24:53 +0700

    new name convention

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-export * from "./navbar"
-=======
+
 
 export * from "./footer"
 
 export * from "./navbar"
 
-<<<<<<< HEAD
 export * from "./banner"
-=======
->>>>>>> a22ba5eb2e353be2c808cc45b9694c43bbc4aa3e
->>>>>>> 0312c23be2125fa210df95d96b04678f3fac0cd2

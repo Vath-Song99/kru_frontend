c76287398b283a84c20ccfcaa4d4dabaa08cdd79
sea porhai <seaporhai@gmail.com>
2024-04-10 16:18:18 +0700

    done responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,15 @@ import React, { useState } from "react";
 import {CreateClass} from "@/components/templates/createClass";
 
 const Page = () => {
-  const [isShowModal, setIsShowModal] = useState<boolean>(false);
+  // const [isShowModal, setIsShowModal] = useState<boolean>(false);
 
-  console.log(isShowModal);
+  // console.log(isShowModal);
 
   return (
     <>
+    
     <CreateClass />
+
     {/* <div className="max-w-full grid">
       <div className="w-full flex justify-center items-center">
         <Navbar setIsShowModal={setIsShowModal} isShowModal={isShowModal} />
--- src/components/templates/createClass/createClass.tsx
+++ src/components/templates/createClass/createClass.tsx
@@ -1,57 +1,59 @@
 import React from 'react'
 import Image from 'next/image'
+import { Button } from '@/components/atoms'
 
 const CreateClass = () => {
     return (
-        <div className= 'container mx-auto  flex justify-center items-center border-white-10 border-2 sm:w-[820px]'>
-
+        <div className='flex justify-center items-center h-screen'>
+            <div className='container mx-auto flex justify-evenly items-center lg:border-white-30 lg:border-2'>
             <div className='hidden lg:block'>
                 <Image
+                    // className='bg-[black]'
                     src="/createclass.png"
                     alt='createclass'
                     width={500}
-                    height={200}>
+                    height={300}>
                 </Image>
             </div>
-
-            <div className='flex flex-col md:[280px]'>
+            <div className='flex md:[280px]  flex-col w-[350px]'>
                 <div className='flex flex-col justify-center items-center container '>
-                <p className='font-bold text-xl lg:text-3xl '>Create Classroom</p>
-                <p className='text-xs w-[350px] h-[60px] mt-4  '>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, molestiae repudiandae! Rem dicta commodi soluta reiciendis aspernatur minus. Aperiam, deserunt!
+                    <p className='font-bold text-2xl lg:text-3xl '>Create Classroom</p>
+                    <p className='text-xs tracking-tight w-[350px] h-[40px] mt-4 lg:text-xs'>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, molestiae repudiandae! Rem dicta commodi soluta reiciendis aspernatur minus. Aperiam, deserunt!
 
-                </p>
+                    </p>
                 </div>
-                <div className='md:w-[200px]'>
-                <form action="">
-                    <label className=' w-[300px]  flex flex-col justify-center items-center  '>
-                        <input type="text"
-                            placeholder='ClassName'
-                            className='border-l-4  border-[#0000e8] mt-4 bg-[#f1f1f1]  w-[250px] h-[30px] px-3 lg:w-[300px]  md:w-[250px]' />
-                        <input type="text"
-                            placeholder='Subject'
-                            className='border-l-4 border-[#0000e8] mt-4 bg-[#f1f1f1]  w-[250px] h-[30px] px-3 lg:w-[300px] md:w-[250px]'
-                        />
-                        <input type="text"
-                            placeholder='Email Address'
-                            className='border-l-4 border-[#0000e8] mt-4 bg-[#f1f1f1]  w-[250px] h-[30px] px-3 lg:w-[300px] md:w-[250px]'
-
-                        />
-                    </label>
-                </form>
+                <div className=' mt-4 md:w-[200px] '>
+                    <form action="">
+                        <label className=' w-[350px] flex flex-col justify-center items-center   '>
+                            <input type="text"
+                                placeholder='ClassName'
+                                className='border-l-4  border-[#0000e8] mt-4 bg-[#f1f1f1]  w-[350px] h-[40px] px-3  md:w-[350px] lg:w-[350px]  outline-none' />
+                            <input type="text"
+                                placeholder='Subject'
+                                className='border-l-4 border-[#0000e8] mt-4 bg-[#f1f1f1]  w-[350px] h-[40px] px-3 lg:w-[350px] md:w-[350px] outline-none'
+                            />
+                            <input type="text"
+                                placeholder='Email Address'
+                                className='border-l-4 border-[#0000e8] mt-4 bg-[#f1f1f1]  w-[350px] h-[40px] px-3 lg:w-[350px] md:w-[350px] outline-none'
+                            />
+                        </label>
+                    </form>
                 </div>
-              
-                <div className='flex flex-col'>
-                    <div className='flex justify-end items-end mt-3 text-xs' >
-                        <p className='underline' >Add your student+</p>
-                    </div>
-                    <div className='flex justify-center items-center'>
-                        <button type="submit"
-                            className='bg-[#7B2CBF] w-[90px] h-[30px] text-white text-xs  md:w-[120px]  md:h-[35px] mb-3 mt-4'>Create Class</button>
-                    </div>
+            <div className=''>
+                <div className='flex justify-end mt-3 text-xs ' >
+                    <a href="" className='underline'>
+                        Add your student+
+                    </a>
+                </div>
+                <div className='flex justify-center items-center'>
+                    <Button className='w-[100px] h-[30px] text-white text-xs  md:w-[100px]  md:h-[30px]  mb-3 mt-4'>  Create Class</Button>
+                    
                 </div>
             </div>
+            </div>
 
+        </div>
         </div>
     )
 }

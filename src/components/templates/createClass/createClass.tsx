641740c51cf0bbeba456f2fd414b3803c61a59ba
sea porhai <seaporhai@gmail.com>
2024-04-10 13:57:06 +0700

    almost done responsive

--- src/components/templates/createClass/createClass.tsx
+++ src/components/templates/createClass/createClass.tsx
@@ -1,10 +1,9 @@
-"use client"
 import React from 'react'
 import Image from 'next/image'
 
 const CreateClass = () => {
     return (
-        <div className='container mx-auto  flex justify-center items-center border-white-10 border-2 sm:w-[220px]'>
+        <div className= 'container mx-auto  flex justify-center items-center border-white-10 border-2 sm:w-[820px]'>
 
             <div className='hidden lg:block'>
                 <Image
@@ -15,28 +14,27 @@ const CreateClass = () => {
                 </Image>
             </div>
 
-            <div className='flex flex-col md: w-[200px]'>
-                <div className='flex flex-col justify-center items-center '>
-                <p className='font-bold text-3xl md: '>Create Classroom</p>
-                <p className='text-xs w-[350px] h-[60px] mt-4 '>
+            <div className='flex flex-col md:[280px]'>
+                <div className='flex flex-col justify-center items-center container '>
+                <p className='font-bold text-xl lg:text-3xl '>Create Classroom</p>
+                <p className='text-xs w-[350px] h-[60px] mt-4  '>
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, molestiae repudiandae! Rem dicta commodi soluta reiciendis aspernatur minus. Aperiam, deserunt!
 
                 </p>
                 </div>
-                <div className='md: w-[300px]'>
+                <div className='md:w-[200px]'>
                 <form action="">
-                    <label className=' w-[480px]  flex flex-col   '>
+                    <label className=' w-[300px]  flex flex-col justify-center items-center  '>
                         <input type="text"
                             placeholder='ClassName'
-                            className='border-l-4  border-[#0000e8] mt-4 bg-[#f1f1f1] w-full h-[40px] px-3 ' />
+                            className='border-l-4  border-[#0000e8] mt-4 bg-[#f1f1f1]  w-[250px] h-[30px] px-3 lg:w-[300px]  md:w-[250px]' />
                         <input type="text"
                             placeholder='Subject'
-                            className='border-l-4 border-[#0000e8] mt-4 bg-[#f1f1f1] w-full h-[40px] px-3'
-
+                            className='border-l-4 border-[#0000e8] mt-4 bg-[#f1f1f1]  w-[250px] h-[30px] px-3 lg:w-[300px] md:w-[250px]'
                         />
                         <input type="text"
                             placeholder='Email Address'
-                            className='border-l-4 border-[#0000e8] mt-4 bg-[#f1f1f1] w-full h-[40px] px-3'
+                            className='border-l-4 border-[#0000e8] mt-4 bg-[#f1f1f1]  w-[250px] h-[30px] px-3 lg:w-[300px] md:w-[250px]'
 
                         />
                     </label>
@@ -44,13 +42,12 @@ const CreateClass = () => {
                 </div>
               
                 <div className='flex flex-col'>
-                    <div className='flex justify-end items-end mt-3' >
+                    <div className='flex justify-end items-end mt-3 text-xs' >
                         <p className='underline' >Add your student+</p>
                     </div>
                     <div className='flex justify-center items-center'>
                         <button type="submit"
-                            className='bg-[#7B2CBF] w-[159px] h-[38px] text-white'
-                        >Create Class</button>
+                            className='bg-[#7B2CBF] w-[90px] h-[30px] text-white text-xs  md:w-[120px]  md:h-[35px] mb-3 mt-4'>Create Class</button>
                     </div>
                 </div>
             </div>

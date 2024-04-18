e751b08444a3612409bfe188eb1502398cb3dc05
nareth347 <nareth441@gmail.com>
2024-04-18 10:58:41 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,23 @@
 /* eslint-disable react/jsx-no-duplicate-props */
 "use client";
 
-import { Footer, Homepage, Navbar } from "@/components";
+import { Footer, Homepage, Navbar, } from "@/components";
 import React, { useState } from "react";
-<<<<<<< HEAD
-import { Modal } from "@/components";
-import { Navbar } from "@/components";
-import SettingProfile from "@/components/templates/SettingProfile";
-=======
->>>>>>> 6a00893ddbfd62d6dd07399d1125255df65bbdf8
 
 const Page = () => {
   const [isShowModal, setIsShowModal] = useState<boolean>(false);
 
 
   return (
-<<<<<<< HEAD
-    <div className="max-w-full ">
-           <div className="w-full flex justify-center items-center">
-                <Navbar isShowModal={isShowModal}  setIsShowModal={setIsShowModal}/>
-                {/* <Modal/> */}
-           </div>     
-           <SettingProfile/>  
-=======
     <div className="max-w-full grid">
       <div className="w-full flex justify-center items-center">
         <Navbar setIsShowModal={setIsShowModal} isShowModal={isShowModal} />
       </div>
-
-      <Homepage />
+      <ForgetPassword/>
 
       <div className="w-full flex justify-center items-start bg-gray-900 mt-10">
         <Footer />
       </div>
->>>>>>> 6a00893ddbfd62d6dd07399d1125255df65bbdf8
     </div>
   );
 };
---
+++ src/components/templates/ForgetPassword.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ForgetPassword = () => {
+    return (
+        <>
+        <div className="bg-green-400 w-[450px] h-480px">
+            df
+        </div>
+        </>
+    );
+} 
+export default ForgetPassword;

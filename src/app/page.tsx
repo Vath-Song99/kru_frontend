3ae5be58f9897af694ad829fb8e70b80849faa59
Smoeury Songvat <vatgaming287@gmail.com>
2024-04-21 18:36:28 +0700

    somthing new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,11 @@
 
 import { Footer, Homepage, Navbar } from "@/components";
 import React, { useState } from "react";
-import { Modal } from "@/components";
-import { Navbar } from "@/components";
-import SettingProfile from "@/components/templates/SettingProfile";
-
 
 const Page = () => {
   const [isShowModal, setIsShowModal] = useState<boolean>(false);
 
-
   return (
-
     <div className="max-w-full grid">
       <div className="w-full flex justify-center items-center">
         <Navbar setIsShowModal={setIsShowModal} isShowModal={isShowModal} />
@@ -24,7 +18,6 @@ const Page = () => {
       <div className="w-full flex justify-center items-start bg-gray-900 mt-10">
         <Footer />
       </div>
-
     </div>
   );
 };
--- src/components/templates/index.ts
+++ src/components/templates/index.ts
@@ -2,6 +2,6 @@ import { Homepage } from "./Homepage";
 import { TeacherList } from "./TeacherList";
 import { About } from "./About";
 import TeachersProfile from "./TeachersProfile";
+import SettingProfile from "./SettingProfile";
 
-export { Homepage, TeacherList, About ,TeachersProfile};
-
+export { Homepage, TeacherList, About, TeachersProfile, SettingProfile };
